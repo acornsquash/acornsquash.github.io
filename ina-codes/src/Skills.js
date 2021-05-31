b682f9ee7d2e13775f20ebfb60ffa1209437d948
inakins <53452799+inakins@users.noreply.github.com>
2021-05-31 14:58:34 -0400

    Chore: added skills

--- ina-codes/src/Skills.js
+++ ina-codes/src/Skills.js
@@ -1,10 +1,14 @@
 import React from "react"
+import JavaScript from "./javascript-39415.png"
+import ReactLogo from "./pngegg.png"
 
 export default class Skills extends React.Component {
     render(){
         return(
             <div className="content">
-            <h1 className="content-box">Here are things I can do!</h1>
+            <h1 className="content-box">I am skilled in: HTML5, CSS, JavaScript, React, React-redux, node.js, express.js, PostgreSQL, Firebase Cloud Storage, </h1>
+            <img src={JavaScript} alt="javascript logo"></img>
+            <img src={ReactLogo} alt="react logo"></img>
             </div>
         )
     }
