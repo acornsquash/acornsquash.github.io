f3a813eaab7f1871ad7156c5c6eff096fd2abcb2
inakins <53452799+inakins@users.noreply.github.com>
2021-05-31 13:45:59 -0400

    button spacing and styling

--- ina-codes/src/Contact.js
+++ ina-codes/src/Contact.js
@@ -1,12 +1,16 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default class Contact extends React.Component {
     render() {
         return(
             <div className="content">
                 <div className="content-box">
-            <h3>Contact Me!</h3>
+            <h3>Contact Me</h3>
             <h5>I love hearing from people!</h5>
+            <h3>Email:</h3><h5>ageeab@gmail.com</h5>
+            <h3><Link to="https://github.com/acornsquash">Github</Link></h3>
+            <h3><Link to="https://www.linkedin.com/in/inaagee/">LinkedIn</Link></h3>
             </div>
             </div>
 
--- ina-codes/src/Nav.js
+++ ina-codes/src/Nav.js
@@ -6,12 +6,13 @@ export default class Nav extends React.Component {
     render(){
         return(
             <div className="nav">
-             <li>
-            <Link to="/"><img src={NameBox} alt="ina"></img></Link>
+            <Link to="/"><img src={NameBox} alt="ina-codes-logo"></img></Link>
+            <div className="button-container">
             <button><Link to="/about">About</Link></button>
             <button><Link to="/skills">Skills</Link></button>
-            <button><Link to="/contact">Contact</Link></button>
-            </li>
+            <button><Link to="/projects">Projects</Link></button>
+            <button id="contact"><Link to="/contact">Contact</Link></button>
+            </div>
             </div>
         )
     }
