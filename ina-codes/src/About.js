0f88bac391a9b0e68ee2f8ebc6e2aec820b3b43c
inakins <53452799+inakins@users.noreply.github.com>
2021-06-02 17:14:47 -0400

    chore: about and contact pages styled, buttons styled

--- ina-codes/src/About.js
+++ ina-codes/src/About.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"
+import React from "react"
 
 export default class About extends React.Component {
     render() {
@@ -6,8 +6,9 @@ export default class About extends React.Component {
         return(
             <div className="content">
             <div className="content-box">
-            
+                <h1 id="dash"> > > > > > </h1>
                 <p>I am a recent graduate of Fullstack Academy, an immersive software engineering bootcamp, currently looking for job opportunities in the (very glamorous) NYC tech world. I love to code and really look forward to doing it full time. My other sparkling passions include poetry, cooking, and long bike rides. </p>
+                <h1 id="dash"> > > > > > </h1>
             </div>
             </div>
         )
--- ina-codes/src/Contact.js
+++ ina-codes/src/Contact.js
@@ -5,12 +5,13 @@ export default class Contact extends React.Component {
     render() {
         return(
             <div className="content">
+                 <h3>Contact Me</h3>
                 <div className="content-box">
-            <h3>Contact Me</h3>
-            <h5>I love hearing from people!</h5>
-            <h3>Email:</h3><h5>ageeab@gmail.com</h5>
-            <h3><Link to="https://github.com/acornsquash">Github</Link></h3>
-            <h3><Link to="https://www.linkedin.com/in/inaagee/">LinkedIn</Link></h3>
+            <h1 id="dash"> > > ></h1>
+            <p>I love hearing from people!</p>
+            <p>ageeab@gmail.com</p>
+            <p><a href="https://github.com/acornsquash">github.com/acornsquash</a></p>
+            <h1 id="dash"> > > ></h1>
             </div>
             </div>
 
