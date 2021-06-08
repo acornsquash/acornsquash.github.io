db36b22171e94d4033aade72b271ece336110def
inakins <53452799+inakins@users.noreply.github.com>
2021-06-08 15:00:42 -0400

    added CV button and new description for moonFaze

--- acornsquash.github.io/src/Home.js
+++ acornsquash.github.io/src/Home.js
@@ -7,10 +7,12 @@ export default class Home extends React.Component {
             <div className="content-home">
                 <div className="content-box-home">
                 <h1 id="dash">> > ></h1>
-            <p>Hi! I'm Ina, recent coding bootcamp grad and aspiring JS developer.</p>
-            <p><img id="me" src={Boat} alt="It's me at work"></img> </p>
-            </div>
-            </div>
-        )
+            <p>Hi! I'm Ina, recent coding bootcamp grad and aspiring JS developer.<a href="https://docs.google.com/document/d/e/2PACX-1vTybdm6gkaQ_kzwjKp9WVR-1hx5NdZ5vKHuZe_lQloslkHKdgB6UfwKXaE4edHLS-JgK2mVlIiEHUfw/pub" target="_blank" rel="noopener noreferrer"><button>Résumé</button></a></p>
+
+<p><img id="me" src={Boat} alt="It's me at work"></img> </p>
+</div>
+</div>
+
+        )       
     }
 }
