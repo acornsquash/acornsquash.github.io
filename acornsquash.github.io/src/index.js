21cc25ed7292fd99b29191d31ddc0902f99d58cd
inakins <53452799+inakins@users.noreply.github.com>
2021-06-08 16:12:36 -0400

    FEAT: responsive design

--- acornsquash.github.io/src/index.js
+++ acornsquash.github.io/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import { Router } from 'react-router-dom'
 import history from './history'
 import App from './App';
-// 
 
 
 ReactDOM.render(
